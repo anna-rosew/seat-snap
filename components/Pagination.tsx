a54bffb3ac0e3611f03409c4b4dc935b85bde697
Anna <annarosewain9@gmail.com>
2024-11-24 15:27:42 +0000

    Created a pagination component

---
+++ components/Pagination.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Button } from "./ui/button";
+import {
+  ChevronFirst,
+  ChevronLast,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface Props {
+  itemCount: number;
+  pageSize: number;
+  currentPage: number;
+}
+
+const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  //count how many page slides we have in total e.g. we have 52 tickets and 25 on each page, that's 2.08
+  const pageCount = Math.ceil(itemCount / pageSize);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  if (pageCount <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
+  return (
+    <div className="mt-4">
+      <div>
+        <Button
+          variant="outline"
+          disabled={currentPage === 1}
+          onClick={() => changePage(1)}
+        >
+          <ChevronFirst />
+        </Button>
+        <Button
+          variant="outline"
+          disabled={currentPage === 1}
+          onClick={() => changePage(currentPage - 1)}
+        >
+          <ChevronLeft />
+        </Button>
+        <Button
+          variant="outline"
+          disabled={currentPage === pageCount}
+          onClick={() => changePage(currentPage + 1)}
+        >
+          <ChevronRight />
+        </Button>
+        <Button
+          variant="outline"
+          disabled={currentPage === pageCount}
+          onClick={() => changePage(pageCount)}
+        >
+          <ChevronLast />
+        </Button>
+      </div>
+      <div>
+        <p>
+          Page {currentPage} of {pageCount}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
