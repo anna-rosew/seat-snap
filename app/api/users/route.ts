7cc1e4c1ac0064bf5757fd8655ccf828e8015060
Anna <annarosewain9@gmail.com>
2024-12-02 20:34:33 +0000

    User POST Api

---
+++ app/api/users/route.ts
@@ -0,0 +1,32 @@
+import { userSchema } from "@/ValidationSchemas/users";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/db";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = userSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextRequest.json(validation.error.format(), { status: 400 });
+  }
+
+  const duplicate = await prisma.user.findUnique({
+    where: {
+      username: body.username,
+    },
+  });
+
+  if (duplicate) {
+    return NextRequest.json({ message: "Duplicate Username" }, { status: 409 });
+  }
+
+  const hashPassword = await bcrypt.hash(body.password, 10);
+  body.password = hashPassword;
+
+  const newUser = await prisma.create({
+    data: { ...body },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
+}
